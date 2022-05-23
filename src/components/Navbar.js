dd88211e55edacd9e7c383bafba9dabb171dace0
Alex <alex.gillespie@live.com>
2022-05-23 18:57:54 -0400

    nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react"
 
 const Navbar = () => {
-  return <h1 className="text-5xl">Alex Gillespie</h1>
+  return (
+    <nav className="flex justify-between items-center mx-auto px-8 py-5 md:px-24 lg:px-24 w-full">
+      <section>
+        <div className="text-5xl font-bold">Alex Gillespie</div>
+      </section>
+      <section className="hidden md:flex space-x-12 items-center">
+        <a href="#work">My Work</a>
+
+        <a href="#resume">Resume</a>
+
+        <a href="#message">
+          <button className="px-6 py-2 bg-blue-700 font-bold">Connect</button>
+        </a>
+      </section>
+    </nav>
+  )
 }
 
 export default Navbar
