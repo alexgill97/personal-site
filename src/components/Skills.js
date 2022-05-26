f16b52c602264e57318b1f0b3aebccb414f4e5b7
Alex <alex.gillespie@live.com>
2022-05-26 15:56:28 -0400

    skills setup

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,32 @@
 import React from "react"
+import tailwind from "../../static/tech-logos/tailwind.svg"
+import { html } from "../../static/index"
 
 const Skills = () => {
   return (
-    <section>
-      <h2 className="secondary-title">Skills</h2>
-      <p class="text-secondary my-6 w-full max-w-3xl">
-        Feel free to to contact me any time, through any method below.
-      </p>
+    <section className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+      <div className="w-full">
+        <h2 className="secondary-title">Skills</h2>
+        <p class="text-secondary my-6 w-full max-w-3xl">
+          Languages and technologies I have utilized throughout my developer
+          journey.
+        </p>
+
+        <div className="container mt-12 grid grid-cols-3 divide-x divide-opacity-50">
+          <div className="flex flex-col items-center ">
+            <h4 className="">Front End</h4>
+            <div>
+              <img src={html} alt="" />
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <h4>Back End</h4>
+          </div>
+          <div>
+            <h4 className="flex justify-center items-center">Tools</h4>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
---
+++ static/index.js
@@ -0,0 +1,11 @@
+import html from "./tech-logos/html.svg"
+export const css = require("./tech-logos/css.svg")
+export const javascript = require("./tech-logos/javascript.svg")
+// export const html = require("./tech-logos/html.svg")
+// import css from "./html.svg"
+// import javascript from "./html.svg"
+// import html from "./html.svg"
+// import html from "./html.svg"
+// import html from "./html.svg"
+
+export { html }
